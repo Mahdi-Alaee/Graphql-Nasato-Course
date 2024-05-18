098c403d33cc4ddbc8cfefa4e923159c25c3126c
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-18 17:34:55 +0330

    practice 2 : get jobs from dataBase

--- practice2_job-board/server/resolvers.js
+++ practice2_job-board/server/resolvers.js
@@ -1,16 +1,7 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    jobs: () => [
-      {
-        id: "1",
-        title: "job 1 title",
-        description: "job 1 description",
-      },
-      {
-        id: "2",
-        title: "job 2 title",
-        description: "job 2 description",
-      },
-    ],
+    jobs: async () => getJobs(),
   },
 };
