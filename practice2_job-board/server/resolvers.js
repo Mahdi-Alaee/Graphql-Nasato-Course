651fccc6386f5cab427e8484babdec10df178cd6
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-18 18:04:05 +0330

    practice 2 : date field resolver

--- practice2_job-board/server/resolvers.js
+++ practice2_job-board/server/resolvers.js
@@ -4,4 +4,10 @@ export const resolvers = {
   Query: {
     jobs: async () => getJobs(),
   },
+  Job: {
+    date: (job) => {
+      console.log(job);
+      return job.createdAt.slice(0, "yyyy-mm-dd".length);
+    },
+  },
 };
