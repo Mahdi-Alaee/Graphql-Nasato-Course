3b983b2a4297cfb3b69b583f65f9bd921b25bb1b
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-05-20 20:10:17 +0330

    practice 2 : set custom error for single job and company query

--- practice2_job-board/server/resolvers.js
+++ practice2_job-board/server/resolvers.js
@@ -1,11 +1,24 @@
 import { getJobs, getJob, getCompanyJobs } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
   Query: {
-    jobs: async () => getJobs(),
-    job: async (_root, { id }) => getJob(id),
-    company: async (_root, { id }) => getCompany(id),
+    jobs: () => getJobs(),
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw new graphqlErrorGenerator(`the job not found with id: "${id}"`);
+      }
+      return job;
+    },
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw graphqlErrorGenerator(`the company not found with id: "${id}"`);
+      }
+      return company;
+    },
   },
   Job: {
     date: (job) => {
@@ -17,3 +30,9 @@ export const resolvers = {
     jobs: (company) => getCompanyJobs(company.id),
   },
 };
+
+function graphqlErrorGenerator(message, code = "INVALID_ID") {
+  return new GraphQLError(message, {
+    extensions: { code },
+  });
+}
