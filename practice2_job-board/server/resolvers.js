b9c3ba6841a740dad83a68ba49e95fd2d1b268af
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-06-01 21:23:57 +0330

    practice 2 : handle limit

--- practice2_job-board/server/resolvers.js
+++ practice2_job-board/server/resolvers.js
@@ -11,7 +11,7 @@ import { GraphQLError } from "graphql";
 
 export const resolvers = {
   Query: {
-    jobs: () => getJobs(),
+    jobs: (_root, {limit}) => getJobs(limit),
     job: async (_root, { id }) => {
       const job = await getJob(id);
       if (!job) {
